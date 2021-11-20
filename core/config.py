b988813d1320c8e8bf8dfc48a5f49e7c81fb1614
ckucktus <paradnik2013@gmail.com>
2021-11-20 06:09:04 +0500

    comment config

--- core/config.py
+++ core/config.py
@@ -1,9 +1,9 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = 'postgresql+asyncpg://test:Xbcnjlkz,l1@ckucktus/fastapi'
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
-    SECRET_KEY: str = 'sugar'
+    #DATABASE_URL: str = 'postgresql+asyncpg://test:Xbcnjlkz,l1@ckucktus/fastapi'
+    #ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
+    #SECRET_KEY: str = 'sugar'
     ALGORITHM: str = 'HS256'
 
 settings = Settings()
